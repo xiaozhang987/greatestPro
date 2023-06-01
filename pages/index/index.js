f1de0a4ed8de31d3b2cc5f6b101052a7e849de3c
myzql <865756980@qq.com>
2023-06-01 14:32:49 +0800

    modified index.js tipwords

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: '304大厨的美食菜谱',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
